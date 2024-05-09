e57b4d47572a22062ac2542c9e8740ea90157698
michalg84 <michalgalka84@gmail.com>
2024-05-09 20:52:44 +0200

    git init

---
+++ src/main/java/com/galka/features/java22/Main.java
@@ -0,0 +1,7 @@
+package com.galka.features.java22;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/galka/features/java22/StreamGatherers.java
@@ -0,0 +1,4 @@
+package com.galka.features.java22;
+
+public class StreamGatherers {
+}

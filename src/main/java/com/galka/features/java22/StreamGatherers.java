011f1b20bc49e8e1880a4b0cb8a4c7c219243110
michalg84 <michalgalka84@gmail.com>
2024-05-14 16:23:08 +0200

    mapConcurrent

--- src/main/java/com/galka/features/java22/StreamGatherers.java
+++ src/main/java/com/galka/features/java22/StreamGatherers.java
@@ -1,5 +1,6 @@
 package com.galka.features.java22;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Gatherers;
 
@@ -25,11 +26,38 @@ public class StreamGatherers {
                 .orElse(0);
     }
 
-    final List<String> incrementalAccumulator(List<Integer> words) {
-        return words.stream()
+    final List<String> incrementalAccumulator(List<Integer> nums) {
+        return nums.stream()
                 .gather(Gatherers.scan(() -> "Acu: ", (a, b) -> a + b))
                 .toList();
 
+    }
+
+    final long parallelOldWay(int[] nums) {
+        long start = System.currentTimeMillis();
+        Arrays.stream(nums).parallel()
+                .boxed()
+                .map(p -> this.invokeExternalService(p))
+                .toArray();
+        return System.currentTimeMillis() - start;
+    }
+
+    final long parallelWithStreamGathererConcurrentMap(int threads, int[] nums) {
+        long start = System.currentTimeMillis();
+        Arrays.stream(nums).parallel()
+                .boxed()
+                .gather(Gatherers.mapConcurrent(threads, p -> this.invokeExternalService(p)))
+                .toArray();
+        return System.currentTimeMillis() - start;
+    }
+
+    private int invokeExternalService(int num) {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return num + 1;
     }
 
 }
